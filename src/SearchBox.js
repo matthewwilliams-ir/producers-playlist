b4d44e70cbb515ba4e49af06df98915e56924b49
Matthew Williams <matthew.williams@impactradius.com>
2018-02-05 11:36:38 +0200

    Refactor

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {AutoComplete} from 'material-ui';
-import getMuiTheme        from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider   from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class SearchBox extends Component {
   constructor(props) {
